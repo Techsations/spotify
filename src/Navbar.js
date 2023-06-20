36f3166b96d636b2421d53daeb82117d5c8ac0c4
Techsations <akandedamilare.t@gmail.com>
2023-06-20 10:07:28 +0100

    Started creating the navbar

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+
+const Navbar = () => {
+  return (
+    <>
+        <div className="nav">
+            <div className="arr p-3">
+            <i className="bi bi-chevron-left me-3 bg-danger fs-5 rounded-circle aro"></i>
+            <i className="bi bi-chevron-right fs-5"></i>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
